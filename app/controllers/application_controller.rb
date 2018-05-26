82875cd89f169ab8f450c8666bffc4a463d78d8a
Arnau Montanes <arnaumonaix@gmail.com>
2018-05-26 02:01:12 +0200

    Added PayPal Express Checkout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_action :set_locale
   before_action :set_num_commands
 
