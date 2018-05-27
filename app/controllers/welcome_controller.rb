c74d816cd49e81f747dc1095dc4bbaf0a847bec6
Arnau Montanes <arnaumonaix@gmail.com>
2018-05-27 13:50:48 +0200

    Added logo and front page images

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
   def index
     @products = Product.find(["1", "2", "3", "4", "5", "6"])
+    @active_products = Product.find(["7", "8", "9"])
   end
 end
